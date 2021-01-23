f86bb8f5496828196972b74d2d774557e7bceecd
yarinmeron <Yarinmeron1@gmail.com>
2021-01-23 10:43:55 +0200

    More changes

--- Server/matrix/MatrixState.java
+++ Server/matrix/MatrixState.java
@@ -5,21 +5,20 @@ import algorithms.State;
 
 public class MatrixState extends State<String> {
 
-	//private String state;
 	public MatrixState(String state) {
 		super(state);
 		this.setCameFrom(null);
 	}
 	@Override
 	public int hashCode() {
-		final int prime = 31;
+		final int code = 31;
 		int result = super.hashCode();
-		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = code * result + ((state == null) ? 0 : state.hashCode());
 		return result;
 	}
 	@Override
-	public boolean equals(State s) {
-		if(this.state.intern()==((String) s.getState()).intern())
+	public boolean equals(State state) {
+		if(this.state.intern()==((String) state.getState()).intern())
 			return true;
 		return false;
 	}
